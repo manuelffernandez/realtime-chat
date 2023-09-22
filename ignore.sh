4276592dfc579b9b355ec3acff246773d60d88b9
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-09-21 21:03:51 -0300

    build(deploy): fix validation on deployment bash script

--- ignore.sh
+++ ignore.sh
@@ -1,6 +1,6 @@
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "dev" || "$VERCEL_GIT_COMMIT_REF" == "main"  ]] ; then
   # Proceed with the build
   echo "Build can proceed"
   exit 1;
