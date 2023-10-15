e8656e0bb9122d409939ce0855bd6792f8ecbeaa
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-15 15:04:06 -0300

    feat(auth): setup next-auth

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,59 @@
+import NextAuth, { type NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { UpstashRedisAdapter } from '@auth/upstash-redis-adapter'
+import { db } from '@/lib/db'
+
+export const authOptions: NextAuthOptions = {
+  adapter: UpstashRedisAdapter(db),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    })
+  ],
+  pages: {
+    signIn: '/signIn'
+  },
+  callbacks: {
+    jwt: async (params) => {
+      // console.log('JWT: ', params)
+      const { token, user } = params
+      const dbUser = await db.get<User>(`user:${token.id}`)
+
+      if (!dbUser) {
+        token.id = user.id
+        return token
+      }
+
+      const { id, name, email, image } = dbUser
+
+      return {
+        id,
+        name,
+        email,
+        image
+      }
+    },
+    session: async (params) => {
+      // console.log('SESSION: ', params)
+      const { session, token } = params
+
+      if (token) {
+        const { id, email, name, image } = token
+        session.user.id = id
+        session.user.email = email as string
+        session.user.name = name as string
+        session.user.image = image
+      }
+
+      return session
+    }
+  },
+  session: {
+    strategy: 'jwt'
+  }
+}
+
+const handler = NextAuth(authOptions)
+
+export { handler as GET, handler as POST }
