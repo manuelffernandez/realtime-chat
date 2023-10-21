56db4196593fee284347480e3b19cf379f8b5c29
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-21 16:58:53 -0300

    feat(signin): create sign in page

---
+++ src/app/(auth)/signIn/page.tsx
@@ -0,0 +1,15 @@
+import GoogleButton from './components/GoogleButton'
+
+const SignInPage = () => {
+  return (
+    <div className='flex min-h-full items-center justify-center px-4 py-12 sm:px-6 lg:px-8'>
+      <div className='flex w-full max-w-md flex-col items-center space-y-8'>
+        <div className='flex flex-col items-center gap-8'>logo</div>
+        <h2 className='mt-6 text-center text-3xl font-bold tracking-tight text-gray-900'>Sign in to your account</h2>
+        <GoogleButton />
+      </div>
+    </div>
+  )
+}
+
+export default SignInPage
