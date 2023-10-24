679481e935dcd6d40523e20074b247c10ff3c179
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-24 18:39:31 -0300

    feat: create redis fetcher function

---
+++ src/helpers/redis.ts
@@ -0,0 +1,26 @@
+type Command = 'zrange' | 'sismember' | 'get' | 'smembers' | 'sadd'
+
+interface RedisRequest {
+  command: Command
+  args: Array<string | number>
+}
+
+export const fetchRedis = async (req: RedisRequest, init: RequestInit | undefined = {}) => {
+  const { command, args } = req
+  const commandUrl = `${process.env.UPSTASH_REDIS_REST_URL ?? ''}/${command}/${args.join('/')}`
+
+  const response = await fetch(commandUrl, {
+    ...init,
+    headers: {
+      Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN ?? ''}`
+    }
+  })
+
+  if (!response.ok) {
+    throw new Error(`Error executing Redis command ${response.statusText}`)
+  }
+
+  const data = await response.json()
+
+  return data.result
+}
