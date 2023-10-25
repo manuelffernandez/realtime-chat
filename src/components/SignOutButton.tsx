b3ed81f4c365fbe3f9d59c6d86258ba731537521
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-25 15:17:19 -0300

    feat: update SignOutButton

--- src/components/SignOutButton.tsx
+++ src/components/SignOutButton.tsx
@@ -2,17 +2,34 @@
 
 import { Button } from '@/components'
 import { routes } from '@/lib/constants/routes.const'
+import { LogOut } from 'lucide-react'
 import { signOut } from 'next-auth/react'
+import { ButtonHTMLAttributes, useState } from 'react'
+import toast from 'react-hot-toast'
 
-const SignOutButton = () => {
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {}
+
+const SignOutButton = (props: Props) => {
+  const [isSigningOut, setIsSigningOut] = useState(false)
+
   return (
     <Button
-      intent='danger'
-      onClick={() => {
-        void signOut({ callbackUrl: routes.pages.home })
+      size='sm'
+      intent='ghost'
+      {...props}
+      isLoading={isSigningOut}
+      onClick={async () => {
+        setIsSigningOut(true)
+        try {
+          await signOut({ callbackUrl: routes.pages.home })
+        } catch (error) {
+          toast.error('There was a problem signing out')
+        } finally {
+          setIsSigningOut(false)
+        }
       }}
     >
-      SignOut
+      {isSigningOut ? null : <LogOut className='h-4 w-4' />}
     </Button>
   )
 }
