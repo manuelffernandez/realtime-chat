4947c171df39540bb3be6cbbf5325ba84761e0ba
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-25 10:11:02 -0300

    feat(components): create Logo component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Button } from './Button'
 export { default as AddFriendButton } from './AddFriendButton'
 export { default as SignOutButton } from './SignOutButton'
+export { default as Logo } from './Logo'
