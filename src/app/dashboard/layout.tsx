04e178c17f5a06778cc36790d2dfab4aabe9f450
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-10-27 14:56:21 -0300

    refactor: update DashboardLayout component

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,32 +1,15 @@
-import { Logo, SignOutButton } from '@/components'
+import { Logo } from '@/components'
 import { nextAuthOptions } from '@/lib/constants/auth.const'
 import { routes } from '@/lib/constants/routes.const'
 import { getServerSession } from 'next-auth'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
-import { type ElementType, type ReactNode } from 'react'
-import { type LucideIcon, UserPlus } from 'lucide-react'
-import Image from 'next/image'
+import { type ReactNode } from 'react'
+import Navbar from './components/Navbar'
 
 interface Props {
   children: ReactNode
 }
-
-interface SidebarOption {
-  id: number
-  name: string
-  href: string
-  Icon: LucideIcon | ElementType
-}
-
-const sidebarOptions: SidebarOption[] = [
-  {
-    id: 1,
-    name: 'Add friend',
-    href: routes.pages.dashboard,
-    Icon: UserPlus
-  }
-]
 
 const DashboardLayout = async (props: Props) => {
   const { children } = props
@@ -40,53 +23,7 @@ const DashboardLayout = async (props: Props) => {
           <Logo className='h-8 w-auto text-indigo-600' />
         </Link>
         <div className='text-xs font-semibold leading-6 text-gray-400'>Your chats</div>
-        <nav className='flex flex-1 flex-col'>
-          <ul role='list' className='flex flex-1 flex-col gap-y-7'>
-            <li>chats that user has</li>
-            <li>
-              <div className='text-xs font-semibold leading-6 text-gray-400'>Overview</div>
-              <ul role='list' className='-mx-2 mt-2 space-y-1'>
-                {sidebarOptions.map((option) => {
-                  const { Icon } = option
-                  return (
-                    <li key={option.id}>
-                      <Link
-                        href={option.href}
-                        className='group flex gap-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600'
-                      >
-                        <span className='-h-6 flex w-6 shrink-0 items-center justify-center rounded-lg border border-gray-200 bg-white text-[0.625rem] font-medium text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600'>
-                          <Icon className='h-4 w-4' />
-                        </span>
-                        <span className='truncate'>{option.name}</span>
-                      </Link>
-                    </li>
-                  )
-                })}
-              </ul>
-            </li>
-            <li className='-mx-6 mt-auto flex items-center'>
-              <div className='flex flex-1 items-center gap-x-4 px-6 py-3 text-sm font-semibold leading-6 text-gray-900'>
-                <div className='relative h-8 w-8 bg-gray-50'>
-                  <Image
-                    fill
-                    referrerPolicy='no-referrer'
-                    className='rounded-full'
-                    src={session.user.image || ''}
-                    alt='Your profile picture'
-                  />
-                </div>
-                <span className='sr-only'>Your profile</span>
-                <div className='flex flex-col'>
-                  <span aria-hidden='true'>{session.user.name}</span>
-                  <span className='text-xs font-normal text-zinc-400' aria-hidden='true' title={session.user.email}>
-                    {session.user.email.length > 20 ? `${session.user.email.slice(0, 20)}...` : session.user.email}
-                  </span>
-                </div>
-                <SignOutButton className='aspect-square h-full' />
-              </div>
-            </li>
-          </ul>
-        </nav>
+        <Navbar session={session} />
       </div>
       {children}
     </div>
