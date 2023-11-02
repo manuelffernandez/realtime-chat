a28ff66031c9f265701899c8e4566e505c753c68
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-11-02 18:13:45 -0300

    feat: extend routes and redis-keys constants

--- src/lib/constants/redis-keys.const.ts
+++ src/lib/constants/redis-keys.const.ts
@@ -2,5 +2,6 @@ export const redisKeys = {
   userById: (id: string) => `user:${id}`,
   idByEmail: (email: string) => `user:email:${email}`,
   friendRequestsById: (id: string) => `user:${id}:friend_requests:incoming`,
-  friendsById: (id: string) => `user:${id}:friends`
+  friendsById: (id: string) => `user:${id}:friends`,
+  chatById: (chatId: string) => `chat:${chatId}:messages`
 }
