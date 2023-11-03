b29a167b1e9bc0e64fb03a20d2715ed591b66f09
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-11-02 23:23:19 -0300

    style: fix some component styles

--- src/app/dashboard/components/FriendRequestSidebarOption.tsx
+++ src/app/dashboard/components/FriendRequestSidebarOption.tsx
@@ -21,7 +21,7 @@ const FriendRequestSidebarOption = (props: Props) => {
       href={routes.pages.friendRequests}
       className='group flex items-center gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600'
     >
-      <div className='flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-400 bg-white text-[0.625rem] font-medium text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600'>
+      <div className='flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-200 bg-white text-[0.625rem] font-medium text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600'>
         <User className='h-4 w-4' />
       </div>
       <p className='truncate'> Friend requests</p>
