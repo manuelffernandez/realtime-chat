474e57d69607e193540f5bad0f4ebf9bfd33b83d
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-11-15 15:41:21 -0300

    style: change icon within FriendRequestSidebarOption

--- src/app/dashboard/_components/FriendRequestSidebarOption.tsx
+++ src/app/dashboard/_components/FriendRequestSidebarOption.tsx
@@ -2,7 +2,7 @@
 
 import { useFriendRequests } from '@/hooks/useFriendRequests'
 import { routes } from '@/lib/constants/routes.const'
-import { User } from 'lucide-react'
+import { UserCheck } from 'lucide-react'
 import Link from 'next/link'
 
 const FriendRequestSidebarOption = () => {
@@ -15,7 +15,7 @@ const FriendRequestSidebarOption = () => {
       className='group flex items-center gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700 hover:bg-gray-50 hover:text-indigo-600'
     >
       <div className='flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border border-gray-200 bg-white text-[0.625rem] font-medium text-gray-400 group-hover:border-indigo-600 group-hover:text-indigo-600'>
-        <User className='h-4 w-4' />
+        <UserCheck className='h-4 w-4' />
       </div>
       <p className='truncate'> Friend requests</p>
       {friendRequests.length > 0 ? (
