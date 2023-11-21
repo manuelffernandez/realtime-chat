b5b86a4fb34c6cb2fd7daf36b6cf855a3bd3b3f4
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2023-11-21 16:48:31 -0300

    chore: update getUser upstash service return type

--- src/services/upstash/get-user.ts
+++ src/services/upstash/get-user.ts
@@ -4,6 +4,6 @@ import { redisKeys } from '@/lib/constants/redis-keys.const'
 export const getUser = async (userId: string) => {
   const { userById } = redisKeys
   const JSONUser = await fetchRedis<string>({ command: 'get', args: [userById(userId)] }, { cache: 'no-store' })
-  const user = JSON.parse(JSONUser) as User
+  const user = JSON.parse(JSONUser) as User | null
   return user
 }
